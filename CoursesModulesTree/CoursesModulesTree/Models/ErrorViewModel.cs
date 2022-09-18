d590003075474af85cbf62ad6c879bddf57335ab
star52 <abramco@yandex.ru>
2022-09-18 14:02:04 +0700

    Сменил шаблон проекта на ASP.NET Core MVC

---
+++ CoursesModulesTree/CoursesModulesTree/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoursesModulesTree.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
