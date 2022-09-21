f81558a7475b7290d00f22f012308c21e14aacdb
star52 <abramco@yandex.ru>
2022-09-21 12:01:54 +0700

    Ступень первая завершена, осталось причесать код в представлении

--- CoursesModulesTree/CoursesModulesTree/Models/ModuleTree.cs
+++ CoursesModulesTree/CoursesModulesTree/Models/ModuleTree.cs
@@ -9,5 +9,24 @@ namespace CoursesModulesTree.Models
     {
         public HashSet<Module> roots;
         public Dictionary<Module, List<Module>> adjacencyList;
+
+        public int getLevel(Module module)
+        {
+            Module parentModule;
+            int level = 1;
+            parentModule = module;
+            while (parentModule.ParentId is not null)
+            {
+                level++;
+                parentModule = getParent(parentModule);
+            }
+            return level;
+        }
+        public Module getParent(Module module)
+        {
+            Module parentModule = null;
+            parentModule = adjacencyList.Keys.Where(x => (x.Id == module.ParentId)).FirstOrDefault();
+            return parentModule;
+        }
     }
 }
